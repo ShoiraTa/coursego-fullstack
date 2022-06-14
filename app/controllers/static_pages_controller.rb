7b65d5cfbf589833a84905b6ae5a88e91543fa1d
shoiraTa <shoira.shakirovna@gmail.com>
2022-06-14 19:12:16 +0400

    feat: add styles

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
-  def landing_page
+  skip_before_action :authenticate_user!, :only => [:index]
+  def index
+    @popular_courses = Course.limit(3)
+    @top_rated_courses = Course.where('title ILIKE ?', '%nursing%')
+    @recent_courses = Course.all.limit(3).order(created_at: :desc)
   end
 
   def privacy_policy
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :courses
-  root 'static_pages#landing_page'
+  root 'static_pages#index'
   get 'privacy_policy', to: 'static_pages#privacy_policy'
 end
