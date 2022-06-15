5259b425598617244fd9a2b3231aa3bf3edf7666
shoiraTa <shoira.shakirovna@gmail.com>
2022-06-15 22:17:09 +0400

    feat: navbar search with ransack

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,13 +3,13 @@ class CoursesController < ApplicationController
 
   # GET /courses or /courses.json
   def index
-    if params[:title]
-      @courses = Course.where('title ILIKE ?', "%#{params[:title]}%") #case-insensitive
-    else
+    # if params[:title]
+    #   @courses = Course.where('title ILIKE ?', "%#{params[:title]}%") #case-insensitive
+    # else
     # @courses = Course.all
-    @q = Course.ransack(params[:q])
-    @courses = @q.result.includes(:user)
-    end
+    @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search)
+    @courses = @ransack_courses.result.includes(:user)
+    # end
   end
 
   # GET /courses/1 or /courses/1.json
