6f485263c55a303a4e9228b2fddbf329ce1913d0
shoiraTa <shoira.shakirovna@gmail.com>
2022-06-15 18:19:37 +0400

    feat: add user trackable

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :courses
+  resources :users, only: [:index]
   root 'static_pages#index'
   get 'privacy_policy', to: 'static_pages#privacy_policy'
 end
