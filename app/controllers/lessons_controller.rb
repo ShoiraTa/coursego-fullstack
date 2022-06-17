b472c5c750f83349c6a9849069fac45896c1d20d
shoiraTa <shoira.shakirovna@gmail.com>
2022-06-17 11:41:02 +0400

    fix: nested lessons inside course

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -8,17 +8,20 @@ class LessonsController < ApplicationController
 
   # GET /lessons/1 or /lessons/1.json
   def show
+    authorize @lesson
   end
 
   # GET /lessons/new
   def new
     @course = Course.friendly.find(params[:course_id])
     @lesson = Lesson.new
-
   end
 
   # GET /lessons/1/edit
   def edit
+    @course = Course.friendly.find(params[:course_id])
+    @lesson = Lesson.friendly.find(params[:id])
+    authorize @lesson
   end
 
   # POST /lessons or /lessons.json
@@ -38,9 +41,10 @@ class LessonsController < ApplicationController
 
   # PATCH/PUT /lessons/1 or /lessons/1.json
   def update
+    authorize @lesson
     respond_to do |format|
       if @lesson.update(lesson_params)
-        format.html { redirect_to lesson_url(@lesson), notice: "Lesson was successfully updated." }
+        format.html { redirect_to course_lesson_url(@lesson.course,@lesson), notice: "Lesson was successfully updated." }
         format.json { render :show, status: :ok, location: @lesson }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,9 +56,9 @@ class LessonsController < ApplicationController
   # DELETE /lessons/1 or /lessons/1.json
   def destroy
     @lesson.destroy
-
+    authorize @lesson
     respond_to do |format|
-      format.html { redirect_to lessons_url, notice: "Lesson was successfully destroyed." }
+      format.html { redirect_to course_lessons_url(@lesson.course, @lesson), notice: "Lesson was successfully destroyed." }
       format.json { head :no_content }
     end
   end
---
+++ app/policies/lesson_policy.rb
@@ -0,0 +1,26 @@
+class LessonPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.all
+    end
+  end
+  def show?
+     @record.course.user_id == @user.id || @user.has_role?(:admin)
+  end
+  def create?
+    # @record.course.user_id == @user.id
+  end
+  def destroy?
+    @record.course.user_id == @user.id
+  end
+
+  def update?
+    @record.course.user_id == @user.id
+  end
+
+  def edit?
+    @record.course.user_id == @user.id
+  end
+
+end
